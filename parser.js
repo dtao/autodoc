7145f73e59a3768c37562ad77bfb95b5f86fda7c
Dan Tao <daniel.tao@gmail.com>
2013-09-13 11:30:07 -0700

    used Mustache to generate specs (bad approach)

---
+++ parser.js
@@ -0,0 +1,31 @@
+// This is a hack to make Lazy.js work.
+// (TODO: update the library to not explicitly require a 'window' object in the browser.)
+this.window = this;
+
+importScripts('lib/esprima/esprima.js', 'lib/lazy.js/lazy.js');
+
+this.onmessage = function(e) {
+  var ast = esprima.parse(e.data, {
+    comment: true,
+    loc: true
+  });
+
+  var examples = Lazy(ast.comments)
+    .map(function(comment) { return comment.value.split('\n'); })
+    .flatten()
+    .map(function(line) {
+      var match = line.match(/^[\s\*]*(.*)\s+=>\s*(.*)\s*$/);
+      if (!match) {
+        return null;
+      }
+
+      return {
+        input: match[1],
+        output: match[2]
+      };
+    })
+    .compact()
+    .toArray();
+
+  postMessage(JSON.stringify({ examples: examples }, null, 2));
+};
