bf655e2d348cbc1251416f9d8a6f4141da401a7b
Dan Tao <daniel.tao@gmail.com>
2013-09-13 10:15:17 -0700

    wrote a little code to associate comments w/ functions

--- worker.js
+++ worker.js
@@ -1,4 +1,8 @@
-importScripts('bower_components/esprima/esprima.js');
+// This is a hack to make Lazy.js work.
+// (TODO: update the library to not explicitly require a 'window' object in the browser.)
+this.window = this;
+
+importScripts('lib/esprima/esprima.js', 'lib/lazy.js/lazy.js');
 
 this.onmessage = function(e) {
   var ast = esprima.parse(e.data, {
@@ -6,5 +10,26 @@ this.onmessage = function(e) {
     loc: true
   });
 
-  postMessage(JSON.stringify(ast, null, 2));
+  var functionDeclarations = Lazy(ast.body)
+    .where({ type: 'FunctionDeclaration' })
+    .groupBy(function(decl) { return decl.loc.start.line; })
+    .toObject();
+
+  var parseComment = function(comment) {
+    var functionDeclaration = functionDeclarations[comment.loc.end.line + 1];
+    if (!functionDeclaration) {
+      return null;
+    }
+
+    return {
+      'value': comment.value,
+      'function': functionDeclaration[0].id.name
+    };
+  };
+
+  var comments = Lazy(ast.comments).map(parseComment)
+    .compact()
+    .toArray();
+
+  postMessage(JSON.stringify(comments, null, 2));
 };
