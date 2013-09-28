9aebd06884bd39be5c16a2f1f9d47fa6f43afe4c
Dan Tao <daniel.tao@gmail.com>
2013-09-27 18:30:59 -0700

    implemented a resizable splitter thing!

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,3 +1,26 @@
 //= require lib/codemirror
 //= require lib/codemirror/mode/javascript
 //= require common
+
+window.addEventListener('load', function() {
+  var left = document.getElementById('left'),
+      right = document.getElementById('right'),
+      splitter = document.getElementById('splitter');
+
+  // My own hacky implementation of a resizable splitter.
+  splitter.addEventListener('drag', function(e) {
+    var x = e.screenX - window.screenLeft;
+
+    splitter.style.left = x + 'px';
+    left.style.right = (window.innerWidth - x) + 'px';
+    right.style.left = x + 'px';
+  });
+
+  window.addEventListener('resize', function() {
+    var x = splitter.offsetLeft;
+
+    splitter.style.left = x + 'px';
+    left.style.right = (window.innerWidth - x) + 'px';
+    right.style.left = x + 'px';
+  });
+});
