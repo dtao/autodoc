b634f5450648331abad2920909bdc448809717bd
Dan Tao <daniel.tao@gmail.com>
2013-09-27 23:16:14 -0700

    updated example text

--- source/example2.js
+++ source/example2.js
@@ -1,34 +1,42 @@
 /**
- * @name redundant.js
+ * @name Project "Breakneck"
  *
  * @fileOverview
- * Check this out. I'm tired of putting all this work into writing code. I want
- * to basically just implement stuff and have docs, specs, and performance
- * benchmarks all generated for me. Isn't it all sort of boilerplate, after all?
- * That's the idea here.
+ * This is just a little something I'm working on to help eliminate a lot of the gruntwork involved
+ * in setting up a new JavaScript project. The rationale here is twofold:
  *
- * This example library re-implements a bunch of functionality that JavaScript's
- * core objects already have. Just for fun.
+ * 1. What we *really* want to be doing as JavaScript devs is **write code**, and not fiddle around
+ *    with a project website. **BUT**:
+ * 2. Meanwhile, it's important for your project to have a nice website if it's going to have the
+ *    kind of impact you want.
  *
- * Right now this tool will generate the following:
+ * How do we solve this problem? We make step 2 happen automatically as a result of step 1!
+ *
+ * Well, this is nowhere near "usable" yet, as a library. But as a hacky little web-based tool, here's
+ * what it can do so far:
  *
  * Documentation
  * -------------
- * See [JsDoc](http://usejsdoc.org/) for details.
+ * API docs will be generated based on the comments above each function. This includes information from
+ * `@param` and `@returns` tags. See [JsDoc](http://usejsdoc.org/) for details. You can use
+ * [Markdown](http://daringfireball.net/projects/markdown/) to format your comments.
  *
  * Specs
  * -----
- * Use the `@examples` tag and separate expected inputs/outputs with '=>'. You
- * can see what I mean in the examples below.
+ * Use the `@examples` tag to define specs in an extremely concise format. You can see what I mean
+ * in the examples below. The result will be a table in the API docs indicating your spec results.
  *
  * Benchmarks
  * ----------
- * Use the `@benchmarks` tag and separate names and implementations with '=>';
- * again, you can see what I'm talking about below.
+ * Use the `@benchmarks` tag to specify cases you want to profile for performance. This will automatically
+ * add a benchmark runner in the appropriate place in the docs, along with a bar chart! (Pick a method and
+ * click 'Run performance benchmarks' to see for yourself!)
  *
- * Oh and also
- * -----------
- * You can use [Markdown](http://daringfireball.net/projects/markdown/) to format your comments.
+ * ---
+ *
+ * *The "library" below is what I'll nickname **redundant.js**, as it re-implements the functionality of
+ * some of JavaScript's core objects (e.g., `Array`, `String`). It is intended only as a convenient way
+ * to illustrate what I'm trying to do here.*
  */
 
 /**
