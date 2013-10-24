bb8194c0e02fbc956299ccd3f1cd2988a340d87d
Dan Tao <daniel.tao@gmail.com>
2013-10-24 16:55:44 -0700

    we don't need no stinkin' source folder

--- source/javascripts/parser.js
+++
@@ -1,8 +0,0 @@
-importScripts(
-  'lib/esprima.js'
-);
-
-this.onmessage = function(e) {
-  var ast  = esprima.parse(e.data, { comment: true, loc: true });
-  postMessage(JSON.stringify(ast, null, 2));
-};
